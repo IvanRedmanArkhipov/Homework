f35068cda7835a35d5b4c570432a431e7e8276f4
IvanArkhipov <redm4nredm4n@gmail.com>
2021-09-26 16:15:47 +0300

    test

---
+++ Homework/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите имя");
+            var name = Console.ReadLine();
+
+            Console.WriteLine($"Привет, {name}, сегодня {DateTime.Now}");
+        }
+    }
+}
