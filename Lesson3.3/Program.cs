42c535cdea0d3e2b1fe6ec789349bf964af38c6b
IvanArkhipov <redm4nredm4n@gmail.com>
2021-10-05 19:06:01 +0300

    test

---
+++ Lesson3.3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Lesson3._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            for (int i = text.Length - 1; i >= 0; i--)
+            {
+                char[] chars = new char[text.Length - 1];
+                for (int n = 1; n <= text.Length - 1; n++)
+                {
+                    chars[n] = text[i];
+                    if (n == text.Length - 1)
+                    {
+                        string string1 = new string(chars);
+                        Console.WriteLine(string1);
+                    }
+                }
+            }
+        }
+    }
+}
