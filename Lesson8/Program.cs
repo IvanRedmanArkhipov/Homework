ed02f67d22c468df8a7d7b883b8edb4f7718544f
IvanArkhipov <redm4nredm4n@gmail.com>
2021-10-22 17:34:46 +0300

    Homework8

---
+++ Lesson8/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Lesson8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (string.IsNullOrEmpty(Properties.Settings1.Default.UserName))
+            {
+                Console.WriteLine("Hello World!");
+                Properties.Settings1.Default.UserName = Console.ReadLine();
+                Properties.Settings1.Default.UserAge = Console.ReadLine();
+                Properties.Settings1.Default.UserWork = Console.ReadLine();
+
+                Properties.Settings1.Default.Save();
+            }
+            else
+            {
+
+            }
+        } 
+    }
+}
