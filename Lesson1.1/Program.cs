ae1fabcae3e7c579585cac8e4653ae56e4ee36ca
IvanArkhipov <redm4nredm4n@gmail.com>
2021-11-01 16:40:39 +0300

    Lesson 1

---
+++ Lesson1.1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Lesson1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Число фибоначи для 0 - " + FNum(0));
+            Console.WriteLine("Число фибоначи для 1 - " + FNum(1));
+            Console.WriteLine("Число фибоначи для 2 - " + FNum(2));
+            Console.WriteLine("Число фибоначи для 3 - " + FNum(3));
+        }
+        static public int FNum(int N)
+        {
+            if (N == 0)
+            {
+                return (0);
+            }
+            if (N == 1)
+            {
+                return (1);
+            }
+            else
+            {
+                return (FNum(N - 2) + FNum(N - 1));
+            }
+        }
+    }
+}
---
+++ Lesson2.2.2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Lesson2._2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Число 2 - " + Num(2));
+            Console.WriteLine("Число 359 - " + Num(359));
+            Console.WriteLine("Число 17 - " + Num(17));
+            Console.WriteLine("Число -3 - " + Num(-3));
+            Console.WriteLine("Число -75 - " + Num(-75));
+            Console.WriteLine("Число 1 - " + Num(1));
+        }
+        static public string Num(int n)
+        {
+            int d = 0;
+            int i = 2;
+            while (i < n)
+            {
+                if (n % i != 0)
+                {
+                    d++;
+                }
+                i++;
+            }
+            if (d == 0)
+            {
+                return ("Простое");
+            }
+            else
+            {
+                return ("Не простое");
+            }
+        }
+    }
+}
