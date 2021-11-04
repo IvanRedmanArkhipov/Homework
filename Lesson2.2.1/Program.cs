36d72356426ccc65358cfd0a71532f4c065732f1
IvanArkhipov <redm4nredm4n@gmail.com>
2021-11-04 15:35:38 +0300

    lesson2

---
+++ Lesson2.2.1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Lesson2._2._1
+{
+    public class Node
+    {
+        public int Value { get; set; }
+        public Node NextNode { get; set; }
+        public Node PrevNode { get; set; }
+    }
+    public interface ILinkedList
+    {
+        int GetCount();
+        void AddNode(int value);
+        void AddNodeAfter(Node node, int value);
+        void AddNodePrev(Node PrevNode, int value);
+        void RemoveNode(int index);
+        void RemoveNode(Node node);
+        Node FindNode(int searchValue);
+    }
+}
+
---
+++ lesson2.2.2n/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace lesson2._2._2n
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums1 = { 1, 2, 5, 17, 21 };
+            Console.WriteLine(BinarySearch(nums1, 17));
+            int[] nums2 = { 1, 2, 5, 9, 17, 21, 199, 313 };
+            Console.WriteLine(BinarySearch(nums2, 9));
+            int[] nums3 = { 1, 7, 2, 5, 17, 21 };
+            Console.WriteLine(BinarySearch(nums3, 2));
+            int[] nums4 = { 1, 2, 5, 17, 21, 3 };
+            Console.WriteLine(BinarySearch(nums4, 3));
+        }
+        //O(1+N)
+        public static int BinarySearch(int[] inputArray, int searchValue)
+        {
+            int min = 0;
+            int max = inputArray.Length - 1;
+            while (min <= max)
+            {
+                int mid = (min + max) / 2;
+                if (searchValue == inputArray[mid])
+                {
+                    return mid;
+                }
+                else if (searchValue < inputArray[mid])
+                {
+                    max = mid - 1;
+                }
+                else
+                {
+                    max = mid + 1;
+                }
+            }
+            return -1;
+        }
+    }
+}
+
